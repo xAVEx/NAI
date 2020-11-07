811e7ebd009b27f47825487cd9002634c635971f
Filip Wrzesien <G4m3over@tlen.pl>
2020-11-07 11:33:27 +0100

    CodinGame

---
+++ The_Descent.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+while True:
+    max = 0
+    maxIndex = -1
+
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+        if mountain_h > max:
+            max = mountain_h
+            maxIndex = i
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    # The index of the mountain to fire on.
+    print(maxIndex)
